c966a5f170e8684565eea1564e1c0ee400442ba5
jeannie.wu <jeannie.wu@mic.com.tw>
2019-05-17 20:00:21 +0800

    Summary   1. Modified B7106 sensor to dbussensor   2. Sync to latest code   3. Modified PSU sensor name
    Tested:
      AC and power on and check power on sensor work normal.
    
    Known issue:
      1. Dimm only support Channel A~D.
      2. power on sensor fail after AC ( workaround fix: restart related service for the first time power on after AC)
      3. Only can show one PSU fru data.

--- meta-mct/meta-s7106/recipes-phosphor/s7106-default-action/files/default-pwm.sh
+++ meta-mct/meta-s7106/recipes-phosphor/s7106-default-action/files/default-pwm.sh
@@ -2,8 +2,25 @@
 
 default_duty=128
 
-echo $default_duty > /sys/class/hwmon/hwmon0/pwm1
-echo $default_duty > /sys/class/hwmon/hwmon0/pwm2
-echo $default_duty > /sys/class/hwmon/hwmon0/pwm3
+#echo $default_duty > /sys/class/hwmon/hwmon0/pwm1
+#echo $default_duty > /sys/class/hwmon/hwmon0/pwm2
+#echo $default_duty > /sys/class/hwmon/hwmon0/pwm3
 echo $default_duty > /sys/class/hwmon/hwmon0/pwm4
 echo $default_duty > /sys/class/hwmon/hwmon0/pwm5
+
+if [ ! -f /var/tmp/acflag ]; then
+
+echo "After ac, restart service to fix sensor"
+systemctl stop xyz.openbmc_project.fansensor.service
+systemctl start xyz.openbmc_project.fansensor.service
+
+
+systemctl stop xyz.openbmc_project.ipmbsensor.service
+systemctl start xyz.openbmc_project.ipmbsensor.service
+
+systemctl stop xyz.openbmc_project.adcsensor.service
+systemctl start xyz.openbmc_project.adcsensor.service
+
+touch /var/tmp/acflag
+fi
+
